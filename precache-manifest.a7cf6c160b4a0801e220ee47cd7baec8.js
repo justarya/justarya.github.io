7b7e2f52a707775bbd57f166ac96ca36aacf590e
justarya <just1arya@gmail.com>
2019-09-29 23:41:02 +0700

    update website

---
+++ precache-manifest.a7cf6c160b4a0801e220ee47cd7baec8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5f27ed76402f4d562f5b2a04a84626b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f9719df11d8772785ecf",
+    "url": "/static/css/main.2560d0bf.chunk.css"
+  },
+  {
+    "revision": "d2f812a636ed2f86a40a",
+    "url": "/static/js/2.52eb3569.chunk.js"
+  },
+  {
+    "revision": "f9719df11d8772785ecf",
+    "url": "/static/js/main.4035f87a.chunk.js"
+  },
+  {
+    "revision": "77ebb0444bb7485b5d9f",
+    "url": "/static/js/runtime-main.ba7d2760.js"
+  }
+]);
